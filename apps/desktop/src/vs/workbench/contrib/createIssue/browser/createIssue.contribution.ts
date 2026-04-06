c2cb39bba2c8427424757fcdae4ff4a8d94f1a6b
Avijit Mandal <amandal@linkedin.com>
2026-04-06 22:40:36 +0530

    Open GitHub issue chooser on create issue button click

--- apps/desktop/src/vs/workbench/contrib/createIssue/browser/createIssue.contribution.ts
+++ apps/desktop/src/vs/workbench/contrib/createIssue/browser/createIssue.contribution.ts
@@ -3,10 +3,12 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import { URI } from '../../../../base/common/uri.js';
 import { Codicon } from '../../../../base/common/codicons.js';
 import { localize2 } from '../../../../nls.js';
 import { Action2, MenuId, registerAction2 } from '../../../../platform/actions/common/actions.js';
 import { ServicesAccessor } from '../../../../platform/instantiation/common/instantiation.js';
+import { IOpenerService } from '../../../../platform/opener/common/opener.js';
 import { IsAuxiliaryWindowContext } from '../../../common/contextkeys.js';
 
 registerAction2(class CreateIssueAction extends Action2 {
@@ -24,7 +26,8 @@ registerAction2(class CreateIssueAction extends Action2 {
 		});
 	}
 
-	override async run(_accessor: ServicesAccessor): Promise<void> {
-		// TODO: implement create issue functionality
+	override async run(accessor: ServicesAccessor): Promise<void> {
+		const openerService = accessor.get(IOpenerService);
+		await openerService.open(URI.parse('https://github.com/workstream-labs/workstreams/issues/new/choose'), { openExternal: true });
 	}
 });
